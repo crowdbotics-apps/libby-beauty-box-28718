c4c30a216a22545a9ba2a35d593469c461d9e3b1
Crowdbotics <team@crowdbotics.com>
2021-07-11 14:30:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/libby_beauty_box_28718/MainActivity.java
@@ -0,0 +1,15 @@
+package com.libby_beauty_box_28718;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "libby_beauty_box_28718";
+  }
+}
